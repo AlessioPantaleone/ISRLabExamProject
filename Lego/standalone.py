1075f0b001c223ba0c7d559ba306a07e8727ee5b
Luchino <lucadelsignore1998@gmail.com>
2021-11-17 12:06:35 +0100

    Primo programma funzionale

--- Lego/standalone.py
+++ Lego/standalone.py
@@ -7,24 +7,25 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-def color_parser():
-    ...
-def distance_parser():
-    ...
+# def color_parser():
+# def distance_parser():
 
 
 if __name__ == "__main__":
 
-    #Import the lego robot
-
-    while 1:
-        #read sensors
-        sensed_color = color_parser()
-        sensed_distance = distance_parser()
-
-        if (sensed_color == "green" and sensed_distance == "near"):
-            ...
+    # Import the lego robot
+    # Set up the Color Sensor.  It is used to detect the red beam when the
+    # neck has moved to its maximum position.
+    # Create your objects here.
+    ev3 = EV3Brick()
+    color_sensor = ColorSensor(Port.S1)
+    distance_sensor = UltrasonicSensor(Port.S2)
+    while True:
+        if color_sensor.color() == Color.RED and distance_sensor.distance() < 100:
+            ev3.speaker.say("Rosso vicino")
+            time.sleep(5)
             # little horn noise
-        if (sensed_color == "red" and sensed_distance == "far"):
-            ...
+        if color_sensor.color() == Color.GREEN and distance_sensor.distance() >= 100:
+            ev3.speaker.say("Verde Lontano")
+            time.sleep(5)
             # big horn noise
