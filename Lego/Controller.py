8fa3110ded135784c795281d50d0fb12ab47db68
Esperozzi <64204197+Esperozzi@users.noreply.github.com>
2021-11-19 12:30:31 +0100

    More addictions to controller

--- Lego/Controller.py
+++ Lego/Controller.py
@@ -1,9 +1,25 @@
+"""
+Controller for the lego EV3 Brick
+"""
 import redis
+
+"""
+Function to send data to redis
+@:param R The Redis instance
+@:param topic The topic you want to publish to
+@:param my_key Name of the key
+@:param value Value of the key
+"""
 
 
 def send_data(R: redis.Redis, topic: str, my_key, value):
     R.set(my_key, value)
     R.publish(topic, my_key)
+
+
+"""
+Controller for the lego EV3 Brick
+"""
 
 
 class Controller:
@@ -16,6 +32,12 @@ class Controller:
         self.color_detected = "NONE"
         self.distance_detected = 0
 
+    """
+    Function to constantly listen to a topic from redis
+    @:param self the controller that is reading data
+    @:param R the Redis instance
+    @:param topic the topic you want to listen to
+    """
     def read_data(self, R: redis.Redis, topic: str):
         pubsub = R.pubsub()
         pubsub.subscribe(topic)
@@ -29,7 +51,13 @@ class Controller:
                     self.distance_detected = value
                 self.act()
 
+    """
+    Function to act based on the belief of the controller
+    """
     def act(self):
+        print('\nState pre  acting: {} with color = {} and distance = {}'.format(self.my_state,
+                                                                              self.color_detected,
+                                                                              self.distance_detected))
         if self.my_state == "Stopped":
             if self.color_detected == "GREEN" and int(self.distance_detected) > 100:
                 self.my_state = "Running"
@@ -38,6 +66,11 @@ class Controller:
             if self.color_detected == "RED" or int(self.distance_detected) < 100:
                 self.my_state = "Stopped"
                 send_data(R, "commands", "Motor_Status", "Stop")
+
+        print('State post acting: {} with color = {} and distance = {}'.format(self.my_state,
+                                                                               self.color_detected,
+                                                                               self.distance_detected))
+
 
 if __name__ == '__main__':
     REDIS_HOST = "localhost"
--- Lego/GimmikExecutableFakeBody.py
+++ Lego/GimmikExecutableFakeBody.py
@@ -1,3 +1,6 @@
+"""
+Gimmik class used for testing to simulate sensors coming from the body of the lego brick
+"""
 
 import redis
 from typing import Dict
@@ -16,27 +19,28 @@ if __name__ == '__main__':
     R = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
     print("My REDIS server version is: ", R.info()['redis_version'])
 
-    # key = "distance_sensor"
-    # to_send = "50"
+    while True:
 
-    try:
-        send_data(R, "sensors", "distance_sensor", "150")
-        send_data(R, "sensors", "color_sensor", "GREEN")
-        sleep(3)
-        send_data(R, "sensors", "distance_sensor", "150")
-        send_data(R, "sensors", "color_sensor", "RED")
-        sleep(3)
-        send_data(R, "sensors", "distance_sensor", "150")
-        send_data(R, "sensors", "color_sensor", "GREEN")
-        sleep(3)
-        send_data(R, "sensors", "distance_sensor", "50")
-        send_data(R, "sensors", "color_sensor", "GREEN")
-        sleep(3)
-        send_data(R, "sensors", "distance_sensor", "50")
-        send_data(R, "sensors", "color_sensor", "RED")
-        sleep(3)
-        send_data(R, "sensors", "distance_sensor", "150")
-        send_data(R, "sensors", "color_sensor", "GREEN")
-    except Exception as e:
-        print("The REDIS server is not available")
-        print(e)
+        try:
+            send_data(R, "sensors", "distance_sensor", "150")
+            send_data(R, "sensors", "color_sensor", "GREEN")
+            sleep(3)
+            send_data(R, "sensors", "distance_sensor", "150")
+            send_data(R, "sensors", "color_sensor", "RED")
+            sleep(3)
+            send_data(R, "sensors", "distance_sensor", "150")
+            send_data(R, "sensors", "color_sensor", "GREEN")
+            sleep(3)
+            send_data(R, "sensors", "distance_sensor", "50")
+            send_data(R, "sensors", "color_sensor", "GREEN")
+            sleep(3)
+            send_data(R, "sensors", "distance_sensor", "50")
+            send_data(R, "sensors", "color_sensor", "RED")
+            sleep(3)
+            send_data(R, "sensors", "distance_sensor", "150")
+            send_data(R, "sensors", "color_sensor", "GREEN")
+        except Exception as e:
+            print("The REDIS server is not available")
+            print(e)
+
+        sleep(30)
