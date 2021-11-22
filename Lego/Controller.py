77f1bfe864631b889e586b462378947969702129
Luchino <lucadelsignore1998@gmail.com>
2021-11-22 17:00:39 +0100

    Altre prove

--- Lego/Controller.py
+++ Lego/Controller.py
@@ -1,7 +1,11 @@
+import time
+
 import redis
+import logging
 
 
 def send_data(R: redis.Redis, topic: str, my_key, value):
+    logging.info(f"Sending data to redis Topic: {topic} , Key:{my_key}, Value:{value}")
     R.set(my_key, value)
     R.publish(topic, my_key)
 
@@ -23,29 +27,34 @@ class Controller:
             if msg["type"] == 'message':
                 source_name = msg["data"]
                 value = R.get(source_name)
+                logging.info(f"New message received: key:{source_name} value:{value}")
                 if source_name == "color_sensor":
                     self.color_detected = value
                     if value != self.color_detected:
+                        logging.info("Belief state updated with new color")
                         self.color_detected = value
                         self.act()
                 if source_name == "distance_sensor":
                     if value != self.distance_detected:
+                        logging.info("Belief state updated with new distance")
                         self.distance_detected = value
                         self.act()
 
     def act(self):
         if self.my_state == "Stopped":
             if self.color_detected == "GREEN" and int(self.distance_detected) > 100:
-                print("Acting to start!")
+                logging.info("Taking action to start motors")
                 self.my_state = "Running"
                 send_data(R, "commands", "Motor_Status", "Ahead")
         if self.my_state == "Running":
             if self.color_detected == "RED" or int(self.distance_detected) < 100:
-                print("Acting to stop!")
+                logging.info("Taking actions to stop motors")
                 self.my_state = "Stopped"
                 send_data(R, "commands", "Motor_Status", "Stop")
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='controllerlogging.txt', level=logging.DEBUG, format='%(levelname)s %(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.info("This is the logging for the lego controller")
     REDIS_HOST = "localhost"
     REDIS_PORT = 6379
     R = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
@@ -53,8 +62,9 @@ if __name__ == '__main__':
 
     while True:
         try:
-            print("My REDIS server version is: ", R.info()['redis_version'])
+            logging.info("connected to redis server")
             C.read_data(R, "sensors")
         except Exception as e:
-            print("The REDIS server is not available")
+            logging.critical("The REDIS server is not available, trying again soon")
             print(e)
+            time.sleep(3)
