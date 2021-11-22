36e94991727df727890f8ea5a960c872684483ba
pantaleone <mrgeostellar@gmail.com>
2021-11-22 10:20:32 +0100

    Reformat file

--- Lego/PhysicalBody.py
+++ Lego/PhysicalBody.py
@@ -2,26 +2,36 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
+
+
 class PhysicalBody:
-    color_sensor= None
-    distance_sensor= None
-    motorL= None
-    motorR=None
+    color_sensor = None
+    distance_sensor = None
+    motorL = None
+    motorR = None
     ev3 = EV3Brick()
-    def __init__(self, Color_Port, Distance_Port, MotorL_Port,MotorR_Port):
+
+    def __init__(self, Color_Port, Distance_Port, MotorL_Port, MotorR_Port):
         self.color_sensor = ColorSensor(Color_Port)
         self.distance_sensor = UltrasonicSensor(Distance_Port)
-        self.motorL= Motor(MotorL_Port)
-        self.motorR= Motor(MotorR_Port)
-    def getDistance(self):
+        self.motorL = Motor(MotorL_Port)
+        self.motorR = Motor(MotorR_Port)
+
+    def get_distance(self):
         return self.distance_sensor.distance()
-    def getColor(self):
+
+    def get_color(self):
         return self.color_sensor.Color()
-    def goForward(self):
+
+    def go_forward(self):
         self.motor.dc(15)
+
     def stop(self):
         self.motor.dc(0)
-    def goBackward(self):
+
+    def go_backward(self):
         self.motor.dc(-15)
+
+
 if __name__ == "__main__":
     print("Misura, invia qualcosa al controller e ricevi qualcosa dal controller")
